794620d780b0646b883254ac912a73df69911441
Leoci Melo <leocij@hotmail.com>
2017-05-22 10:56:35 -0300

    Criada entidades Aluno e Curso com relacionamento

--- src/main/java/ljmtp220520170937/entity/Curso.java
+++ src/main/java/ljmtp220520170937/entity/Curso.java
@@ -1,5 +1,64 @@
 package ljmtp220520170937.entity;
 
-public class Curso {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Curso implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+
+	@OneToMany(mappedBy = "curso_id")
+	private List<Aluno> alunos;
+
+	public Curso() {
+		super();
+	}
+
+	public Curso(Long id, String nome, List<Aluno> alunos) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+
+	@Override
+	public String toString() {
+		return "Curso [id=" + id + ", nome=" + nome + ", alunos=" + alunos + "]";
+	}
 
 }
