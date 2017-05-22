15d221bfd54b6e187af973b4b043edaf92f2049c
Leoci Melo <leocij@hotmail.com>
2017-05-22 09:44:47 -0300

    Primeiro commit

---
+++ src/main/java/ljmtp220520170937/Ljmtp220520170937Application.java
@@ -0,0 +1,12 @@
+package ljmtp220520170937;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ljmtp220520170937Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ljmtp220520170937Application.class, args);
+	}
+}
---
+++ src/test/java/ljmtp220520170937/Ljmtp220520170937ApplicationTests.java
@@ -0,0 +1,16 @@
+package ljmtp220520170937;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ljmtp220520170937ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
